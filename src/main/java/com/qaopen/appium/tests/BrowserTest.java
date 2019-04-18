af6985955045f30929d9b9343fdc827f94d7fb60
Alexey Voloshyn <vastland@gmail.com>
2019-04-18 18:55:18 +0300

    Updated for QAOpen 2019

---
+++ src/main/java/com/qaopen/appium/tests/BrowserTest.java
@@ -0,0 +1,87 @@
+package com.qaopen.appium.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Test class for browser
+ */
+public class BrowserTest {
+
+    // Instance of WebDriver
+    private WebDriver driver;
+
+    // Instance of WebDriverWait
+    private WebDriverWait wait;
+
+    //
+    // WebElements
+    //
+    @FindBy(xpath = "//input[@name='q']")
+    private WebElement searchInput;
+
+    /**
+     * Set up method
+     *
+     * @throws IOException
+     */
+    @Before
+    public void setUp() throws IOException {
+
+        // Capabilities
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        // Set device type
+        desiredCapabilities.setCapability("device", "Android");
+
+        // Set browser type. Leave it empty if it's app
+        desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
+
+        // Set name of the device
+        desiredCapabilities.setCapability("deviceName", "MI 5");
+
+        // Set platform name
+        desiredCapabilities.setCapability("platformName", "Android");
+
+        // Initialize driver
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+
+        // Initialize WebDriverWait
+        wait = new WebDriverWait(driver, 10);
+
+        // Initialize WebElements
+        PageFactory.initElements(driver, this);
+    }
+
+    /**
+     * Open Chrome, go to Google and send 'GlobalLogic' to the search
+     */
+    @Test
+    public void testBrowserTest() {
+        driver.get("https://google.com.ua");
+        searchInput.sendKeys("Open QA 2019");
+        searchInput.sendKeys(Keys.ENTER);
+    }
+
+    /**
+     * Tear down method. Quit driver
+     */
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
